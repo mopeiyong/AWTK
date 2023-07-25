8732337288be8890475e92d593445b8a2f31d2a1
mopeiyong <398428200@qq.com>
2023-07-25 17:44:22 +0800

    awtk-examples

---
+++ MusicPlayer-Demo/src/custom_widgets/photo_frame/cover_view.c
@@ -0,0 +1,76 @@
+﻿/**
+ * File:   cover_view.h
+ * Author: AWTK Develop Team
+ * Brief:  cover_view
+ *
+ * Copyright (c) 2018 - 2019  Guangzhou ZHIYUAN Electronics Co.,Ltd.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * License file for more details.
+ *
+ */
+
+/**
+ * History:
+ * ================================================================
+ * 2018-04-05 Li XianJing <xianjimli@hotmail.com> created
+ *
+ */
+
+#include "tkc/mem.h"
+#include "cover_view.h"
+
+static ret_t cover_view_on_paint_children(widget_t* widget, canvas_t* c) {
+  cover_view_t* cover_view = COVER_VIEW(widget);
+  vgcanvas_t* vg = canvas_get_vgcanvas(c);
+  rect_t r;
+  rect_t r_save;
+  rect_t r_vg_save;
+  return_value_if_fail(cover_view != NULL, RET_BAD_PARAMS);
+
+
+  /* 裁剪子控件的话，需要注意保存和还原 canvas 和 vg 这两个画布，*/
+  /* 因为子控件可能会修改任意一个画布的裁剪区或者其他的配置，有概率会导致其他的控件的绘图不正常 */
+  /* 如果创建新的裁剪区的话，需要和之前的裁剪区做交集，让新的裁剪区必须在旧的裁剪区中 */
+  canvas_save(c);
+  canvas_get_clip_rect(c, &r_save);
+  if (vg != NULL) {
+    vgcanvas_save(vg);
+    r_vg_save = rect_init(vg->clip_rect.x, vg->clip_rect.y, vg->clip_rect.w, vg->clip_rect.h);
+  }
+
+  r = rect_init(c->ox, c->oy, widget->w, widget->h);
+  r = rect_intersect(&r, &r_save);
+  canvas_set_clip_rect(c, &r);
+  if (vg != NULL) {
+    vgcanvas_clip_rect(vg, r.x, r.y, r.w, r.h);
+  }
+
+  widget_on_paint_children_default(widget, c);
+  if (vg != NULL) {
+    vgcanvas_clip_rect(vg, r_vg_save.x, r_vg_save.y, r_vg_save.w, r_vg_save.h);
+    vgcanvas_restore(vg);
+  }
+  canvas_set_clip_rect(c, &r_save);
+  canvas_restore(c);
+
+  return RET_OK;
+}
+
+TK_DECL_VTABLE(cover_view) = {.size = sizeof(cover_view_t),
+                              .type = WIDGET_TYPE_VIEW,
+                              .parent = TK_PARENT_VTABLE(widget),
+                              .create = cover_view_create,
+                              .on_paint_children = cover_view_on_paint_children};
+
+widget_t* cover_view_create(widget_t* parent, xy_t x, xy_t y, wh_t w, wh_t h) {
+  return widget_create(parent, TK_REF_VTABLE(cover_view), x, y, w, h);
+}
+
+widget_t* cover_view_cast(widget_t* widget) {
+  return_value_if_fail(WIDGET_IS_INSTANCE_OF(widget, cover_view), NULL);
+
+  return widget;
+}
---
+++ README.md
@@ -0,0 +1,89 @@
+# AWTK 示例使用说明
+
+#### 项目介绍：
+
+- HelloWorld.Xml-Demo：最简单的入门 Demo，演示如何使用 XML 方式设计界面
+- HelloDesigner-Demo：最简单的入门 Demo，可以用AWTK Designer打开
+- CleanAir-Demo：新风系统演示 Demo
+- Chart-Demo：图表演示 Demo，包含仪表、饼图、曲线图、柱状图
+- MusicPlayer-Demo：音乐播放器演示 Demo
+- SmartWatch-Demo：智能手表演示 Demo
+
+
+
+#### 入门文档：
+
+- zlgopen/awtk-docs/AWTK开发实践
+
+
+
+#### Windows 使用方法：
+
+1. 确保已经安装好 VS2015（Visual C++）、Python2.7（x64）、SCons3.0.1 等工具
+
+2. 将 awtk 和 awtk-examples 下载到同一个文件夹下
+
+3. 修改 awtk 文件夹的 awtk_config.py 脚本使用 AGGE 渲染方式
+
+   > AGGE 渲染方式兼容性最好，可以兼容 Windows、Linux、macOS 以及各种嵌入式平台
+
+   ```
+   NANOVG_BACKEND='AGGE'
+   ```
+
+4. 进入 awtk 文件夹，在命令行中输入 scons 编译 awtk
+
+   ```
+   C:\zlgopen\awtk> scons
+   ```
+
+5. 进入 awtk-examples 中某个项目文件夹，在命令行中输入 scons 编译项目
+
+   ```
+   C:\zlgopen\awtk-examples\HelloWorld-Demo> scons
+   ```
+
+6. 进入项目的 bin 文件夹启动 demo
+
+   ```
+   C:\zlgopen\awtk-examples\HelloWorld-Demo\bin> demo.exe
+   ```
+
+
+
+
+#### AWorksOS 使用方法：
+
+1. 打开已经配置好的 AWTK（Eclipse） 工程
+
+2. 将上面某个 Demo 文件夹直接拷贝或拖放到工程 app 目录下
+
+3. 将 app 中其他 Demo 文件夹从工程中屏蔽（Exclude from Build）
+
+   ```
+   1. 在工程数中选中要屏蔽的 Demo 文件夹
+   2. 右键菜单选中 Resource Configurations -> Exclude from Build...
+   3. 弹出对话框点击 Select All -> OK
+   ```
+
+4. 编译工程
+
+5. 下载到板子调试运行
+
+
+
+> Eclipse 工程中的 awtk-demos 是 AWTK 的内置的示例，包含各种控件的使用方法
+>
+> 更详细的使用方法，请参考入门文档
+
+
+
+#### 运行效果截图：
+
+![CleanAir-Demo](docs/images/cleanair_main.png)
+
+![Chart-Demo](docs/images/chart_main.png)
+
+![MusicPlayer-Demo](docs/images/musicplayer_main.png)
+
+![SmartWatch-Demo](docs/images/smartwatch_main.png)
